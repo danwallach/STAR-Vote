2d6a478630a9d4cbf84cb682dbe8d7c25e70e8a3
Martinnikoltchev <martin.nikoltchev@gmail.com>
2013-06-12 11:35:54 -0500

    added untested functionality for multiple ballots through ballot manager class

---
+++ supervisor/model/BallotManager.java
@@ -0,0 +1,11 @@
+package supervisor.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: martinnikol
+ * Date: 6/12/13
+ * Time: 10:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BallotManager {
+}
