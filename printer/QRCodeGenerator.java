13b9b36fdc391344e40b116bf3393dc6ab0bf7ff
Martinnikoltchev <martin.nikoltchev@gmail.com>
2013-06-13 14:18:23 -0500

    added reciet printing unctionality, reciet prints with a QRCode but the printout is offset

---
+++ printer/QRCodeGenerator.java
@@ -0,0 +1,11 @@
+package printer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: martinnikol
+ * Date: 6/13/13
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QRCodeGenerator {
+}
