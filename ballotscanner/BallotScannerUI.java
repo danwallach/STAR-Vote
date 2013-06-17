1992251c33e2edfd7da0f925583f7fd41991f521
Mitchell Douglass <mrdouglass95@gmail.com>
2013-06-17 15:47:13 -0500

    Finished most of the graphical components of the GUI. TODO: write methods that interface the UI with the different states of the ballot scanner machine

--- ballotscanner/BallotScannerUI.java
+++ ballotscanner/BallotScannerUI.java
@@ -4,10 +4,13 @@ import votebox.AuditoriumParams;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -19,19 +22,27 @@ import java.util.Date;
  */
 public class BallotScannerUI extends JFrame {
 
-    private JPanel electionInfoPanel;
-    private JPanel userInfoPanel;
+    private JPanel mainPanel;
 
-    private ImageIcon logo;
-    private ImageIcon acceptedIcon;
-    private ImageIcon rejectedIcon;
+    private ElectionInfoPanel electionInfoPanel;
+    private UserInfoPanel userInfoPanel;
 
-    private JLabel logoLabel;
+    private BufferedImage logo;
+    private BufferedImage rejectImage;
+    private BufferedImage acceptImage;
+    private BufferedImage waitingImage;
+    private BufferedImage responseImage;
+
     private JLabel scanResultLabel;
     private JLabel dateLabel;
 
     private DateFormat dateFormat;
     private Date date;
+
+    private String electionName;
+
+    private Font fontBig   = new Font("Arial Unicode", Font.BOLD, 20);
+    private Font fontSmall = new Font("Arial Unicode", Font.PLAIN, 18);
 
     public BallotScannerUI(String electionName){
         super("STAR-Vote Ballot Scanner");
@@ -45,40 +56,175 @@ public class BallotScannerUI extends JFrame {
         dateFormat = new SimpleDateFormat("MMMM d, y");
         date = new Date();
 
+        this.electionName = electionName;
+
         try{
-            logo = new ImageIcon(ImageIO.read(new File("images/logo.png")));
-            logoLabel = new JLabel(logo);
+            logo = ImageIO.read(new File("images/logo.png"));
         }catch(IOException ioe){
-            System.out.println("BallotScannerUI: Could not locate logo Icon");
-            logoLabel =  new JLabel("Votebox");
+            System.out.println("BallotScannerUI: Could not locate logo image");
+            logo = null;
         }
 
         try{
-            acceptedIcon = new ImageIcon(ImageIO.read(new File("images/logo.png")));
-
+            acceptImage = ImageIO.read(new File("images/accept_ballot.png"));
         }catch(IOException ioe){
-            System.out.println("BallotScannerUI: Could not locate logo Icon");
+            System.out.println("BallotScannerUI: Could not locate accept image");
+            acceptImage = null;
         }
 
-        try{rejectedIcon = new ImageIcon(ImageIO.read(new File("images/logo.png")));}
-        catch(IOException ioe){System.out.println("BallotScannerUI: Could not locate logo Icon");}
+        try{
+            rejectImage = ImageIO.read(new File("images/reject_ballot.png"));
+        }catch(IOException ioe){
+            System.out.println("BallotScannerUI: Could not locate reject image");
+            rejectImage = null;
+        }
 
-        electionInfoPanel = new JPanel();
-        electionInfoPanel.add(logoLabel);
-        electionInfoPanel.add(new JLabel(electionName));
-        electionInfoPanel.add(new JLabel(dateFormat.format(date)));
+        try{
+            waitingImage = ImageIO.read(new File("images/waiting_ballot.png"));
+        }catch(IOException ioe){
+            System.out.println("BallotScannerUI: Could not locate waiting image");
+            waitingImage = null;
+        }
+
+        responseImage = waitingImage;
+
+        userInfoPanel = new UserInfoPanel();
+        electionInfoPanel = new ElectionInfoPanel();
+
+        JPanel
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(electionInfoPanel);
+        mainPanel.add(userInfoPanel);
+        add(mainPanel);
+        pack();
+    }
+
+    private class ElectionInfoPanel extends JPanel{
+
+        private JPanel logoPanel;
+        private JPanel infoPanel;
+
+        public ElectionInfoPanel(){
+            setPreferredSize(new Dimension(600,200));
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            setBorder(BorderFactory.createEtchedBorder());
+
+            logoPanel = new JPanel(){
+                public void paintComponent(Graphics g){
+                    super.paintComponent(g);
+                    if(logo != null){
+                        g.drawImage(logo, 100, 5, 400, 90, null);
+                    }
+                }
+            };
+
+            logoPanel.setPreferredSize(new Dimension(600, 100));
+
+            infoPanel = new JPanel();
+            infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+            infoPanel.setPreferredSize(new Dimension(600, 100));
+
+            JLabel info;
+
+            info = new JLabel(electionName);
+            info.setFont(fontBig);
+            info.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            infoPanel.add(info);
+
+            info = new JLabel(dateFormat.format(date));
+            info.setFont(fontSmall);
+            info.setAlignmentX(Component.CENTER_ALIGNMENT);
+            infoPanel.add(info);
+
+            info = new JLabel("Welcome to this Ballot Scanner Console");
+            info.setFont(fontSmall);
+            info.setAlignmentX(Component.CENTER_ALIGNMENT);
+            infoPanel.add(info);
+
+            add(logoPanel);
+            add(infoPanel);
+            //add(Box.createRigidArea(new Dimension(600, 50)));
+        }
+    }
+
+    private class UserInfoPanel extends JPanel{
+
+        private JPanel messagesPanel;
+        private JPanel scanResponsePanel;
+
+        private int messageLines;
+
+        ArrayList<String> messages;
+
+        public UserInfoPanel(){
+            setPreferredSize(new Dimension(600,300));
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            setBorder(BorderFactory.createEtchedBorder());
+
+            scanResponsePanel = new JPanel(){
+                public void paintComponent(Graphics g){
+                    super.paintComponent(g);
+                    if(responseImage != null){
+                        g.drawImage(responseImage, 100, 15, 400, 90, null);
+                    }
+                }
+            };
+            scanResponsePanel.setBorder(BorderFactory.createEtchedBorder());
+            scanResponsePanel.setPreferredSize(new Dimension(600, 100));
+
+            messages = new ArrayList<String>();
+
+            messagesPanel = new JPanel();
+            messagesPanel.setPreferredSize(new Dimension(600, 150));
+            messagesPanel.setLayout(new BoxLayout(messagesPanel, BoxLayout.Y_AXIS));
+            messagesPanel.setBorder(BorderFactory.createEtchedBorder());
+
+            clearMessages();
+
+            add(messagesPanel);
+            add(scanResponsePanel);
+        }
+
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            messagesPanel.removeAll();
+
+            int gap = (200 - messages.size()*20)/2;
+
+            messagesPanel.add(Box.createRigidArea(new Dimension(600,gap/3)));
+            for(int i=0; i<messages.size(); i++){
+                JLabel lab = new JLabel(messages.get(i));
+                lab.setFont(fontSmall);
+                lab.setAlignmentX(Component.CENTER_ALIGNMENT);
+                messagesPanel.add(lab);
+            }
+            messagesPanel.add(Box.createRigidArea(new Dimension(600,gap*2/3)));
+
+            messagesPanel.validate();
+        }
+
+        public void clearMessages(){
+            messages.clear();
+            messagesPanel.removeAll();
+        }
+
+        public void addMessage(String mes){
+           messages.add(messages.size(), mes);
+        }
     }
 
     public void displayPromptScreen(){
-        setVisible(true);
-        userInfoPanel = new JPanel();
-        userInfoPanel.add(logoLabel);
-        userInfoPanel.add(new JLabel("Welcome to the Ballot Scanning Console"));
-        userInfoPanel.add(new JLabel("To Cast Your Vote, Place Your Barcode Under the Scanner"));
+        userInfoPanel.clearMessages();
+        userInfoPanel.addMessage("This is a Ballot Scanning Console");
+        userInfoPanel.addMessage("Place Ballot Under Scanner to Cast Ballot");
         updateFrame();
     }
 
-    private void updateFrame(){
+    public void updateFrame(){
         electionInfoPanel.repaint();
         userInfoPanel.repaint();
     }
