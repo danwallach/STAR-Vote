a8a34ea3182908193f238ca67bb131637588c1e6
Mitchell Douglass <mrdouglass95@gmail.com>
2013-06-18 11:23:39 -0500

    replaced Timer in rejected and accepted with custom delay Thread

--- ballotscanner/BallotScanner.java
+++ ballotscanner/BallotScanner.java
@@ -300,12 +300,13 @@ public class BallotScanner{
                 //If this event corresponds with our last scanned ballot, display a confirmation message
                 if(lastFoundBID.equals(event.getBID())){
                     frame.displayBallotAcceptedScreen(lastFoundBID);
-                    new Timer(5000, new ActionListener() {
-                        public void actionPerformed(ActionEvent e) {
-                            System.out.println("Calling Scan Ballot After accepted ballot");
+                    new Thread(){
+                        public void run(){
+                            long start = System.currentTimeMillis();
+                            while(System.currentTimeMillis() - start < 5000);
                             scanBallot();
                         }
-                    }).start();
+                    }.start();
                 }
 
             }
@@ -317,12 +318,13 @@ public class BallotScanner{
                 //If our ballot was rejected, display a message
                 if(lastFoundBID.equals(event.getBID())){
                     frame.displayBallotRejectedScreen();
-                    new Timer(5000, new ActionListener() {
-                        public void actionPerformed(ActionEvent e) {
-                            System.out.println("Calling Scan Ballot After rejected ballot");
+                    new Thread(){
+                        public void run(){
+                            long start = System.currentTimeMillis();
+                            while(System.currentTimeMillis() - start < 5000);
                             scanBallot();
                         }
-                    }).start();
+                    }.start();
                 }
             }
 
