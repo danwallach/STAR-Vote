e98a9a69a748e4e639f172f7a4374deef652ff6c
martin nikoltchev <martinnikoltchev@gmail.com>
2013-06-19 11:36:59 -0500

    fixing the unknown machine issue

---
+++ votebox/events/IdentifyMachineEvent.java
@@ -0,0 +1,11 @@
+package votebox.events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: martinnikol
+ * Date: 6/19/13
+ * Time: 11:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IdentifyMachineEvent {
+}
---
+++ votebox/events/PollMachinesEvent.java
@@ -0,0 +1,11 @@
+package votebox.events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: martinnikol
+ * Date: 6/19/13
+ * Time: 10:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PollMachinesEvent {
+}
