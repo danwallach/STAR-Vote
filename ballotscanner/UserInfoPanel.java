4542008e80221a33dd8c41e39a844456fabc5327
Matthew Kindy II <mpk2@rice.edu>
2014-05-23 10:00:02 -0500

    Added comments to and deleted old code from UserInfoPanel.java. Added TODOs for the second code pass. Made some easy code fixes (like for -> foreach)

--- ballotscanner/UserInfoPanel.java
+++ ballotscanner/UserInfoPanel.java
@@ -12,12 +12,14 @@ public class UserInfoPanel extends JPanel {
     private JPanel scanResponsePanel;
     private final BallotScannerUI context;
     private Font fontSmall = new Font("Arial Unicode", Font.PLAIN, 18);
+    private ArrayList<String> messages;
 
-    private int messageLines;
-
-    ArrayList<String> messages;
-
+    /**
+     * This constructor sets up the UserInfoPanel corresponding to the state of the BallotScannerUI.
+     * @param ballotScannerUI is the @BallotScannerUI that corresponds to this @UserInfoPanel
+     */
     public UserInfoPanel(BallotScannerUI ballotScannerUI){
+
         context = ballotScannerUI;
         setPreferredSize(new Dimension(600,300));
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -26,11 +28,16 @@ public class UserInfoPanel extends JPanel {
         scanResponsePanel = new JPanel(){
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
-                if(context.state.getStateImage() != null){
+
+                /* TODO fix state */
+                /* If the state is defined, draw the window corresponding to the right state */
+                if(context.state.getStateImage() != null)
                     g.drawImage(context.state.getStateImage(), 100, 15, 400, 90, null);
-                }
+
             }
         };
+
+        /* Set up for scanning */
         scanResponsePanel.setBorder(BorderFactory.createEtchedBorder());
         scanResponsePanel.setPreferredSize(new Dimension(600, 100));
 
@@ -38,46 +45,69 @@ public class UserInfoPanel extends JPanel {
 
         messagesPanel = new JPanel();
         messagesPanel.setPreferredSize(new Dimension(600, 150));
-        //messagesPanel.setLayout(new BoxLayout(messagesPanel, BoxLayout.Y_AXIS));
         messagesPanel.setBorder(BorderFactory.createEtchedBorder());
 
+        /* Clear out any old messages */
         clearMessages();
 
         add(messagesPanel);
         add(scanResponsePanel);
     }
 
+    /* TODO this method seems a lot more specific than it should be... part of this could be a separate method. */
+    /**
+     * Adds a graphics component to @UserInfoPanel and adds messages to the panel as well.
+     * @param g the graphics component to be added to the @UserInfoPanel
+     */
     public void paintComponent(Graphics g){
+
         super.paintComponent(g);
 
         messagesPanel.removeAll();
 
+        /* Enforce spacing between components */
         int gap = (200 - messages.size()*20)/2;
-
         messagesPanel.add(Box.createRigidArea(new Dimension(600,gap/3)));
-        for(int i=0; i<messages.size(); i++){
-            JLabel lab = new JLabel(messages.get(i));
+
+        /* For all messages in the ArrayList... */
+        for(String message : messages){
+
+            /* Create a new JLabel in the centre of the screen with the current message */
+            JLabel lab = new JLabel(message);
             lab.setHorizontalAlignment(SwingConstants.CENTER);
             lab.setFont(fontSmall);
 
+            /* TODO feel like this needs to be changed... pretty ugly, and I think there's a simpler way to do this. */
+            /* Define the font size */
             int textHeight = 0;
-            textHeight += (fontSmall.getStringBounds(messages.get(i),new FontRenderContext(new AffineTransform(), true, true))).getHeight();
+            textHeight += (fontSmall.getStringBounds(message,new FontRenderContext(new AffineTransform(), true, true))).getHeight();
 
+            /* Set the overall size of the JLabel */
             lab.setPreferredSize(new Dimension (messagesPanel.getWidth(), textHeight));
-            //lab.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            /* Add the JLabel to the panel */
             messagesPanel.add(lab);
         }
+
+        /* Continue enforcing spacing between components */
         messagesPanel.add(Box.createRigidArea(new Dimension(600,gap*2/3)));
 
         messagesPanel.validate();
     }
 
+    /**
+     * Clears all messages from the messages ArrayList and the JPanel
+     */
     public void clearMessages(){
         messages.clear();
         messagesPanel.removeAll();
     }
 
+    /**
+     * Adds a message to @messages which will later be added to the @UserInfoPanel
+     * @param mes the message to be added to @messages
+     */
     public void addMessage(String mes){
-        messages.add(messages.size(), mes);
+        messages.add(mes);
     }
 }
