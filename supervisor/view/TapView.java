31cbec5b1d0a1e5f633bd41b5fe03c8bd4cf8672
Matt Bernhard <mdb92nc@gmail.com>
2014-05-25 17:37:15 -0500

    Commented TapMachineView and the Supervisor's main view.

--- supervisor/view/TapView.java
+++ supervisor/view/TapView.java
@@ -10,40 +10,62 @@ import java.awt.*;
  * whether or not the Tap is responsive and on the network.
  *
  * @author Matt Bernhard
- * @version 0.0.1
- *          Date: 7/29/13
  */
 public class TapView extends AMachineView{
 
+    /** The mini-model for the Tap */
     private TapMachine machine;
 
+    /** the label that will contain the ribbon */
     private JLabel statusLabel;
 
+    /** A color to show that the machine is active */
     private static Color activeColor = new Color(0,170,0);
 
+    /**
+     * A constructor for this mini-view
+     *
+     * @param tapMachine a mini-machine representation of the Tap, from the supervisor's eyes
+     */
     public TapView(TapMachine tapMachine){
 
+        /* First set up the mini-machine */
         machine = tapMachine;
 
+        /* Now create the ribbon */
         statusLabel = new JLabel();
         setLayout(new BorderLayout());
         add(statusLabel, BorderLayout.CENTER);
 
+        /* Set the border, for aesthetic appeal */
         setBorder(BorderFactory.createEtchedBorder());
 
+        /* Update so everything gets filled in nicely */
         updateView();
     }
 
+    /**
+     * Allows different Tap machines to be swapped in and out.
+     *
+     * @param m the new machine
+     */
     public void setMachine(TapMachine m){
         machine = m;
     }
 
+    /**
+     * Update the view depending on whether or not the machine is online or not
+     */
     public void updateView() {
 
+        /* If the machine is non-null and online, set the color to green and display the corresponding message */
         if(machine != null && machine.isOnline()){
             setBackground(activeColor);
             statusLabel.setText("             TAP  Active             ");
-        }else{
+        }
+
+        /* Otherwise set the color to gray and show the corresponding message */
+        else{
             setBackground(Color.GRAY);
             statusLabel.setText("             TAP Offline             ");
         }
