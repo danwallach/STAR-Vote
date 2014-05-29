f68d1a65aa5a13495dca50713e46065c27d326ed
Matt Bernhard <mpk2@rice.edu>
2014-05-29 15:28:00 -0500

    Comments/revision to StraightTicket.java, WriteInCardElement.java

--- votebox/middle/ballot/StraightTicket.java
+++ votebox/middle/ballot/StraightTicket.java
@@ -44,8 +44,10 @@ import votebox.middle.driver.UnknownUIDException;
  */
 public class StraightTicket extends ACardStrategy {
 
-	//Local interface for help in cleaning up some repeated code
-	//  Still relatively hackish, but a lot better than copy/paste
+    /**
+     * Local interface for help in cleaning up some repeated code.
+     * Still relatively hackish, but a lot better than copy/paste.
+     */
 	private interface IStub{
 		public void invoke(SelectableCardElement sce) throws CardStrategyException;
 	}
@@ -53,16 +55,13 @@ public class StraightTicket extends ACardStrategy {
 	/**
 	 * For each party, there is a mapping here from the party name to a list of
 	 * candidates who belong to that party.
-	 * 
 	 */
 	private HashMap<String, ArrayList<SelectableCardElement>> _partyAffiliations = null;
 
 	/**
 	 * This is a mapping of each party to the button who does the work of
 	 * straight ticket for that party.
-	 * 
 	 */
-
 	private HashMap<String, SelectableCardElement> _buttons = new HashMap<String, SelectableCardElement>();
 
 	/**
@@ -80,70 +79,60 @@ public class StraightTicket extends ACardStrategy {
 	 * list here so we know who to talk to later when the voter expresses intent
 	 * for straight-ticket. Also, we need to make sure that everyone who claims
 	 * that he belongs to a party, when deselected, also deselects the straight
-	 * ticket button for his party. < summary>
+	 * ticket button for his party. </summary>
 	 */
 	private void assignParties() throws CardStrategyException {
+
 		_partyAffiliations = new HashMap<String, ArrayList<SelectableCardElement>>();
-		// For each CardElement in the ballot, we're going to check for a party
-		// affiliation and then add it to our data structure
+
+		/*
+		  For each CardElement in the ballot, we're going to check for a party
+		  affiliation and then add it to our data structure.
+		*/
 		for (Card c : _card.getParent().getCards()) {
-			for (SelectableCardElement ce : c.getElements()) {
-				// If:
-				// (1) We're not looking at the card who is delegating to this
-				// strategy (That is the straight ticket card)
-				// (2) The cardelement has set its value for "party"
-				if ((c != _card)
-						&& (ce.getProperties().contains(Properties.PARTY))) {
-					String party = null;
-					try {
-						party = ce.getProperties().getString(Properties.PARTY);
-					} catch (IncorrectTypeException e) {
-						throw new CardStrategyException(
-								"There was an issue getting the party affiliation of candidate with UID: "
-										+ ce.getUniqueID(), e);
+
+            for (SelectableCardElement ce : c.getElements()) {
+
+				/*
+				  If:
+				   (1) We're not looking at the card who is delegating to this
+				       strategy (That is the straight ticket card)
+				   (2) The cardElement has set its value for "party"
+				*/
+				if ((c != _card) && (ce.getProperties().contains(Properties.PARTY))) {
+
+                    String party;
+
+                    try { party = ce.getProperties().getString(Properties.PARTY); }
+                    catch (IncorrectTypeException e) {
+						throw new CardStrategyException("There was an issue getting the party affiliation of candidate with UID: " + ce.getUniqueID(), e);
 					}
-					ArrayList<SelectableCardElement> list = null;
-					// If we've encountered this party before, just add to its
-					// list.
+
+					ArrayList<SelectableCardElement> list;
+
+                    /*
+                      -If we've encountered this party before, just add to its list.
+					  -If we have not encountered this party before, create a new list,
+					   Assign it to the party, and then add to that list.
+                    */
 					if (_partyAffiliations.containsKey(party))
 						list = _partyAffiliations.get(party);
-					// If we have not encountered this party before, create a
-					// new list,
-					// Assign it to the party, and then add to that list.
 					else {
 						list = new ArrayList<SelectableCardElement>();
 						_partyAffiliations.put(party, list);
 					}
-					;
 
 					list.add(ce);
-
-                    //This is commented out because it forced any selection that was not
-                    //of the previously selected straight ticket party to deselect all
-                    //candidates for that party, which is kind of bad.
-					/*// Make sure that when this button is deselected, the
-					// straight ticket
-					// button is also deselected
-					SelectableCardElement straightTicketButton = null;
-					if ((straightTicketButton = _buttons.get(party)) != null)
-						ce.getDeselectedEvent()
-								.addObserver(
-										straightTicketButton
-												.getDeselectHandler());*/
 				}
-				// If, instead, !(1), we -are- looking at the straight ticket
-				// card, add the element to the straight ticket buttons mapping
-				else if ((c == _card) && (ce instanceof SelectableCardElement)
-						&& (ce.getProperties().contains(Properties.PARTY))) {
-					try {
-						_buttons
-								.put(ce.getProperties().getString(
-										Properties.PARTY),
-                                        ce);
-					} catch (IncorrectTypeException e) {
-						throw new CardStrategyException(
-								"There was an issue getting the party affiliation of straight ticket button with UID: "
-										+ ce.getUniqueID(), e);
+				/*
+				  If, instead, !(1), we -are- looking at the straight ticket
+				  card, add the element to the straight ticket buttons mapping
+				*/
+				else if ((c == _card) && (ce != null) && (ce.getProperties().contains(Properties.PARTY))) {
+
+                    try { _buttons.put(ce.getProperties().getString(Properties.PARTY),ce); }
+                    catch (IncorrectTypeException e) {
+                        throw new CardStrategyException("There was an issue getting the party affiliation of straight ticket button with UID: " + ce.getUniqueID(), e);
 					}
 				}
 			}
@@ -152,11 +141,12 @@ public class StraightTicket extends ACardStrategy {
 
     /**
      * Groups Card Elements with their party affiliations
+     *
      * @return a Map from party-affiliation String to list of affiliated Card elements
      * @throws CardStrategyException
      */
-	private HashMap<String, ArrayList<SelectableCardElement>> getPartyAffiliations()
-			throws CardStrategyException {
+	private HashMap<String, ArrayList<SelectableCardElement>> getPartyAffiliations() throws CardStrategyException {
+
 		if (_partyAffiliations == null)
 			assignParties();
 
@@ -166,28 +156,29 @@ public class StraightTicket extends ACardStrategy {
 	/**
 	 * When this element is selected, before delegating to the radio button
 	 * strategy, we need to explicitly select all the elements who belong to the
-	 * same party that this selected button belongs to. 
-	 * @param element This is the button who was selected. Check the party of
-	 * this element and then select everyone who belongs to that party.
+	 * same party that this selected button belongs to.
+     *
+	 * @param element   This is the button who was selected. Check the party of
+	 *                  this element and then select everyone who belongs to that party.
+     *
+     * @return          the result of RadioButton.Singleton.select on element
+     *
+     * @throws CardStrategyException if there isn't a UI element associated with the ballot
+     *                               element or some other view error during .select()
 	 */
-	public boolean select(SelectableCardElement element)
-			throws CardStrategyException {
+	public boolean select(SelectableCardElement element) throws CardStrategyException {
+
 		String party = getParty(element);
-		iterateOverAnd(party, new IStub(){
+
+        iterateOverAnd(party, new IStub(){
 			public void invoke(SelectableCardElement sce) throws CardStrategyException{
-				try {
-					sce.getParentCard().getParent().getViewAdapter().select(
-							sce.getUniqueID());
-				} catch (UnknownUIDException e) {
-					throw new CardStrategyException(
-							"When executing straight ticket behavior, an error occurred: "
-									+ e.getMessage()
-									+ " This probably means that there is no ToggleButton defined that corresponds with a particular ballot element.",
-							e);
+				try { sce.getParentCard().getParent().getViewAdapter().select(sce.getUniqueID()); }
+                catch (UnknownUIDException e) {
+					throw new CardStrategyException("When executing straight ticket behavior, an error occurred: " + e.getMessage()
+									                + " This probably means that there is no ToggleButton defined that corresponds"
+                                                    + " with a particular ballot element.", e);
 				} catch (SelectionException e) {
-					throw new CardStrategyException(
-							"When executing straight ticket behavior, an error occurred in the view: "
-									+ e.getMessage(), e);
+					throw new CardStrategyException("When executing straight ticket behavior, an error occurred in the view: " + e.getMessage(), e);
 				}
             }
 		});
@@ -198,61 +189,66 @@ public class StraightTicket extends ACardStrategy {
 	/**
 	 * Extracts the party associated with the given element.
 	 * 
-	 * @param element
-	 * @return associated party
-	 * @throws CardStrategyException 
+	 * @param element   the element selected from which to get the party
+	 * @return          the associated party
+     *
+	 * @throws CardStrategyException if the straight ticket party is not declared
+     *                               as a String
 	 */
 	protected String getParty(SelectableCardElement element) throws CardStrategyException{
-		try {
-			return element.getProperties().getString(Properties.PARTY);
-		} catch (IncorrectTypeException e) {
-			throw new CardStrategyException(
-					"When extracting the straight ticket party of an element in a straight ticket card, we found that the property was declared to be the wrong type. Declare this property as a string.",
-					e);
+
+		try { return element.getProperties().getString(Properties.PARTY); }
+        catch (IncorrectTypeException e) {
+			throw new CardStrategyException("When extracting the straight ticket party of an element in a straight ticket card, "
+                                            + "we found that the property was declared to be the wrong type. Declare this property"
+                                            + " as a string.", e);
 		}
-	}//getParty
+	}
 	
 	/**
 	 * Traverses elements associated with the given party, and invokes
 	 * the provided stub on them.
-	 * @param party - party in question
-	 * @param stub - stub to invoke
-	 * @throws CardStrategyException 
+     *
+	 * @param party     party in question
+	 * @param stub      stub to invoke
+     *
+	 * @throws CardStrategyException
 	 */
 	protected void iterateOverAnd(String party, IStub stub) throws CardStrategyException{
-		List<SelectableCardElement> parties = null;
+
+        List<SelectableCardElement> parties;
 		
 		if ((parties = getPartyAffiliations().get(party)) != null) {
-			// Select all the candidates who belong to this party.
+
+			/* Select all the candidates who belong to this party. */
 			for (SelectableCardElement sce : parties)
 				stub.invoke(sce);
-		}//if
-	}//iterateOverAnd
+		}
+	}
 	
 	/**
 	 * When this element is deselected, before delegating to the radio button
 	 * strategy, we need to explicitly deselect all the elements who belong to
-	 * the same party that this selected button belongs to. 
-	 * @param element This is the button who was deselected. Check the party of
-	 * this element and then deselect everyone who belongs to that party.
+	 * the same party that this selected button belongs to.
+     *
+	 * @param element   the button who was deselected.
+     *
+     * @throws CardStrategyException
 	 */
-	public boolean deselect(SelectableCardElement element)
-			throws CardStrategyException {
+	public boolean deselect(SelectableCardElement element) throws CardStrategyException {
 		
 		String party = getParty(element);
+
 		iterateOverAnd(party, new IStub(){
+
 			public void invoke(SelectableCardElement sce) throws CardStrategyException {
-				try {
-					sce.getParentCard().getParent().getViewAdapter().deselect(
-							sce.getUniqueID(), false);
-				} catch (UnknownUIDException e) {
-					throw new CardStrategyException(
-							"When executing straight ticket behavior, could not find an element in the view corresponding to a particular ballot ID"
-									+ e.getMessage(), e);
+
+				try { sce.getParentCard().getParent().getViewAdapter().deselect(sce.getUniqueID(), false); }
+                catch (UnknownUIDException e) {
+					throw new CardStrategyException("When executing straight ticket behavior, could not find an element " +
+                                                    "in the view corresponding to a particular ballot ID." + e.getMessage(), e);
 				} catch (DeselectionException e) {
-					throw new CardStrategyException(
-							"When executing straight ticket behavior, an error occurred in the view: "
-									+ e.getMessage(), e);
+					throw new CardStrategyException("When executing straight ticket behavior, an error occurred in the view: " + e.getMessage(), e);
 				}
 			}
 		});
