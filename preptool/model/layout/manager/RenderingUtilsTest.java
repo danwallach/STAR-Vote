d364563737fc474b6639a17c00825f0fdfa82c2e
Matt Bernhard <mdb92nc@gmail.com>
2014-05-30 10:27:11 -0500

    Adding a test suite for testing rendering of components.

---
+++ preptool/model/layout/manager/RenderingUtilsTest.java
@@ -0,0 +1,18 @@
+package preptool.model.layout.manager;
+
+import junit.framework.TestCase;
+
+/**
+ * A test suite to verify the functionality of miscellaneous things in the RenderingUtils package
+ */
+public class RenderingUtilsTest extends TestCase {
+
+    public void setUp(){
+
+    }
+
+    public void testDrawBox(){
+
+    }
+
+}
