d726d1ef86b09c36e6661970277548296a5fa6b0
Matthew Kindy II <mpk2@rice.edu>
2014-06-03 13:35:52 -0500

    Commented/revised/TODO'd Layout.java, LayoutParser.java, LayoutParserException.java

--- votebox/middle/view/Layout.java
+++ votebox/middle/view/Layout.java
@@ -62,19 +62,17 @@ public class Layout {
     /**
      * This is the public constructor for Layout.
      * 
-     * @param properties
-     *            These are the properties for this layout that were parsed out
-     *            of the layout xml.
-     * @param pages
-     *            These are the pages that constitute the layout.
-     * @param drawables
-     * 	          a map of UIDs to lists of the associated drawable elements.
+     * @param properties    the properties for this layout that were parsed out
+     *                      of the layout xml.
+     * @param pages         the pages that constitute the layout.
+     * @param drawables     a map of UIDs to lists of the associated drawable elements.
      */
-    public Layout(Properties properties, List<RenderPage> pages,
-            HashMap<String, LinkedList<IDrawable>> drawables) {
+    public Layout(Properties properties, List<RenderPage> pages, HashMap<String, LinkedList<IDrawable>> drawables) {
+
         _properties = properties;
         _pages = pages;
         _uidMap = drawables;
+
         setChildren();
     }
 
@@ -82,14 +80,13 @@ public class Layout {
      * Set the parent reference for all the children.
      */
     private void setChildren() {
-        for (RenderPage rp : _pages)
-            rp.setParent( this );
+        for (RenderPage rp : _pages) rp.setParent(this);
     }
 
     /**
      * This is the getter for _properties.
      * 
-     * @return _properties.
+     * @return  _properties.
      */
     public Properties getProperties() {
         return _properties;
@@ -98,7 +95,7 @@ public class Layout {
     /**
      * This is the getter for _pages
      * 
-     * @return _pages
+     * @return  _pages
      */
     public List<RenderPage> getPages() {
         return _pages;
@@ -108,10 +105,8 @@ public class Layout {
      * Call this method to check if a UID is used for more than one drawable in
      * this layout.
      * 
-     * @param uid
-     *            Check for this UID
-     * @return This method returns true if the given uid has more than one
-     *         binding, or false if not.
+     * @param uid   the UID for which to check
+     * @return      true if the given uid has more than one binding, or false if not.
      */
     public boolean isDuplicate(String uid) {
         return _uidMap.containsKey( uid ) && _uidMap.get( uid ).size() > 1;
@@ -120,49 +115,43 @@ public class Layout {
     /**
      * Look up the bound instances of a drawable by UID.
      * 
-     * @param uid
-     *            The drawable with this UID will be looked up
-     * @return This method returns a list containing the drawable instances
-     *         bound to a given UID, where lower orders indices in the list
-     *         denote more recently bound instances.
-     * @throws UnknownUIDException
-     *             This method throws if the given uid is not bound to any
-     *             drawable.
+     * @param uid   the UID that will be in the drawable to be looked up
+     * @return      a list containing the drawable instances bound to a given UID, where
+     *              lower orders indices in the list denote more recently bound instances.
+     *
+     * @throws UnknownUIDException if the given UID is not bound to any drawable.
      */
     public List<IDrawable> lookup(String uid) throws UnknownUIDException {
-        if (_uidMap.containsKey( uid ))
-            return _uidMap.get( uid );
+
+        if (_uidMap.containsKey(uid))
+            return _uidMap.get(uid);
         else
-            throw new UnknownUIDException( uid );
+            throw new UnknownUIDException(uid);
     }
 
     /**
      * Lookup which page a particular drawable exists in.
      * 
-     * @param uid
-     *            The drawable with this UID will be looked up.
-     * @return This method returns the page number that holds the drawable named
-     *         by the given UID.
-     * @throws UnknownUIDException
-     *             This method throws if the UID doesn't exist in the layout.
-     * @throws DuplicateUIDException
-     *             This method throws if the given UID exists more than once in
-     *             the layout.
+     * @param uid   the UID that will be in the drawable to be looked up
+     * @return      the page number that holds the drawable named by the given UID.
+     *
+     * @throws UnknownUIDException if the UID doesn't exist in the layout.
+     * @throws DuplicateUIDException if the given UID exists more than once in the layout.
      */
-    public int lookupPage(String uid) throws UnknownUIDException,
-            DuplicateUIDException {
-        if (!_uidMap.containsKey( uid ))
-            throw new UnknownUIDException( uid );
+    public int lookupPage(String uid) throws UnknownUIDException, DuplicateUIDException {
 
-        if (isDuplicate( uid ))
-            throw new DuplicateUIDException( uid );
+        if (!_uidMap.containsKey(uid))
+            throw new UnknownUIDException(uid);
+
+        if (isDuplicate(uid))
+            throw new DuplicateUIDException(uid);
 
         for (int lcv = 0; lcv < _pages.size(); lcv++)
-            for (IDrawable d : _pages.get( lcv ).getChildren())
-                if (d.getUniqueID().equals( uid ))
+            for (IDrawable d : _pages.get(lcv).getChildren())
+                if (d.getUniqueID().equals(uid))
                     return lcv;
 
-        throw new RuntimeException( "Control can never get here." );
+        throw new RuntimeException("Control can never get here.");
     }
 
     /**
@@ -170,35 +159,27 @@ public class Layout {
      * number given is out of range, this method will adhere to the nearest
      * index extrema.
      * 
-     * @param pagenum
-     *            This is the page number that wishes to be drawn.
-     * @param view
-     *            This is the view that the page wishes to be drawn to.
+     * @param pagenum   the page number that wishes to be drawn.
+     * @param view      the view that the page wishes to be drawn to.
      */
     public void draw(int pagenum, IView view) {
-        try {
-            _pages.get( pagenum ).draw( view );
-        }
+
+        try { _pages.get(pagenum).draw(view); }
         catch (IndexOutOfBoundsException e) {
-            throw new BallotBoxViewException(
-                    "You have attempted to draw a page which does not exist:"
-                            + e.getMessage(), e );
+            throw new BallotBoxViewException("You have attempted to draw a page which does not exist:" + e.getMessage(), e );
         }
     }
 
-    public void initFromViewManager(int pagenum, IViewManager vmadapter,
-            IBallotLookupAdapter lookupadapter, IAdapter ballotadapter,
+    public void initFromViewManager(int pagenum, IViewManager vmadapter, IBallotLookupAdapter lookupadapter, IAdapter ballotadapter,
             IViewFactory viewadapter, IBallotVars ballotvars) {
-        _pages.get( pagenum ).initFromViewManager( vmadapter, lookupadapter,
-            ballotadapter, viewadapter, ballotvars );
 
+        _pages.get(pagenum).initFromViewManager(vmadapter, lookupadapter, ballotadapter, viewadapter, ballotvars);
     }
 
-    public void initFromViewManager(ViewManager manager,
-            IBallotLookupAdapter lookupAdapter, IAdapter ballotAdapter,
+    public void initFromViewManager(ViewManager manager, IBallotLookupAdapter lookupAdapter, IAdapter ballotAdapter,
             IViewFactory factory, IBallotVars ballotvars) {
+
         for (RenderPage rp : _pages)
-            rp.initFromViewManager( manager, lookupAdapter, ballotAdapter,
-                factory, ballotvars );
+            rp.initFromViewManager(manager, lookupAdapter, ballotAdapter, factory, ballotvars);
     }
 }
