8305551b81cf2f8ddbd7a0f1ec2b32bb85e24917
Matt Bernhard <mpk2@rice.edu>
2014-06-05 14:39:46 -0500

    Commented test files ApplicationTest.java, IntegrationTest.java

--- web-server/test/IntegrationTest.java
+++ web-server/test/IntegrationTest.java
@@ -17,12 +17,18 @@ public class IntegrationTest {
      */   
     @Test
     public void test() {
+
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+
             public void invoke(TestBrowser browser) {
+
+                /* Go to the local host and check the pageSource */
                 browser.goTo("http://localhost:3333");
                 assertThat(browser.pageSource()).contains("Your new application is ready.");
             }
+
         });
+
     }
   
 }
