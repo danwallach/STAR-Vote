42d9c4c168b6c572fd675e76c083c37dd00ad01b
Matt Bernhard <mdb92nc@gmail.com>
2014-07-28 17:28:49 -0500

    Removed .xml files

--- web-server/target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ web-server/target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -24,9 +24,11 @@ object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,
 
     /**/
     def apply():play.api.templates.HtmlFormat.Appendable = {
-        _display_ {
+        _display_ {import helper.twitterBootstrap._
 
-Seq[Any](_display_(Seq[Any](/*1.2*/main("STAR-Vote Audit Server")/*1.32*/ {_display_(Seq[Any](format.raw/*1.34*/("""
+
+Seq[Any](format.raw/*2.1*/("""
+"""),_display_(Seq[Any](/*3.2*/main("STAR-Vote Audit Server")/*3.32*/ {_display_(Seq[Any](format.raw/*3.34*/("""
 
 <center><h1>Welcome to the Star-Vote Ballot Verifying Site</h1></center>
 <h2 style="test-align: center">How this website works</h2>
@@ -47,7 +49,7 @@ Seq[Any](_display_(Seq[Any](/*1.2*/main("STAR-Vote Audit Server")/*1.32*/ {_disp
 <p>
     On this website, you can confirm that your cast ballot was included in tallying the results of the election. You can also review a challenged ballot, which will include an image of your ballot to be cross referenced with the copy you took home. To confirm or challenge, simply scan the QR code or type in the accompanying URL from your ballot receipt. Alternatively, click the confirm or challenge links in the menu above to perform these tasks manually.
 </p>
-""")))})),format.raw/*22.2*/("""
+""")))})),format.raw/*24.2*/("""
 """))}
     }
     
@@ -60,11 +62,11 @@ Seq[Any](_display_(Seq[Any](/*1.2*/main("STAR-Vote Audit Server")/*1.32*/ {_disp
 }
                 /*
                     -- GENERATED --
-                    DATE: Mon Jul 28 17:08:41 CDT 2014
+                    DATE: Mon Jul 28 17:20:01 CDT 2014
                     SOURCE: /home/mdb12/Workspace/STAR-Vote/web-server/app/views/index.scala.html
-                    HASH: 96267d47ad13b514f2e119cf4b54aaac41155cb7
-                    MATRIX: 864->1|902->31|941->33|2520->1581
-                    LINES: 29->1|29->1|29->1|50->22
+                    HASH: 044959d5a50feb3f3221a0b8404d69dabc9b263c
+                    MATRIX: 888->34|924->36|962->66|1001->68|2580->1616
+                    LINES: 30->2|31->3|31->3|31->3|52->24
                     -- GENERATED --
                 */
             
