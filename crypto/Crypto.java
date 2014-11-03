0918514267c2875c735dada9b84f81911e087d61
Matt Bernhard <mpk2@rice.edu>
2014-11-03 15:38:46 -0600

    Beginning of new Crypto class

---
+++ crypto/Crypto.java
@@ -0,0 +1,19 @@
+package crypto;
+
+/**
+ * Created by Matthew Kindy II on 11/3/2014.
+ */
+public class Crypto {
+
+    public byte[] decrypt(){ }
+
+    public byte[] encrypt(){ }
+
+
+
+
+
+
+
+
+}
