b0881db624bf1f98cd48d63e1079f46e77d9b4f6
Matt Bernhard <mpk2@rice.edu>
2014-11-10 15:57:02 -0600

    Wrote skeleton code for tests for CryptoType and ByteCrypto. Introduced Vote/BallotCrypto

---
+++ crypto/BallotCrypto.java
@@ -0,0 +1,7 @@
+package crypto;
+
+/**
+ * Created by Matthew Kindy II on 11/9/2014.
+ */
+public class BallotCrypto {
+}
---
+++ crypto/test/ElGamalCryptoTypeTest.java
@@ -0,0 +1,26 @@
+package crypto.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Matthew Kindy II on 11/9/2014.
+ */
+public class ElGamalCryptoTypeTest extends TestCase {
+
+    protected void setUp() throws Exception {
+
+    }
+
+    public void testDecrypt(){ /* Test without private key and also with */ }
+
+    public void testEncrypt(){ /* Test without public key and also with */ }
+
+    public void testLoadPrivateKey(){ /* Test both implementations with good and bad keys */ }
+
+    public void testLoadPublicKey(){ /* Test both implementations with good and bad keys */ }
+
+    public void testLoadKeys(){ /* Test both implementations with good and bad keys */ }
+
+    public void testExpectedProperties(){ /* Test various decrypt/encrypt setups to see if properties are working */}
+
+}
