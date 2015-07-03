53f3a50ef2b34d93c8ae6f6c870f46cccc488183
Matt Bernhard <mpk2@rice.edu>
2015-07-02 19:05:46 -0500

    Fixing project structure

--- ballotscanner/BallotScanner.java
+++ ballotscanner/BallotScanner.java
@@ -10,7 +10,6 @@ import votebox.events.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
